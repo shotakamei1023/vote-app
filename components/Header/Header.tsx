b9f8a942204be48da00f7e81d1bf3e65761d1d59
shotam <67728298+shotakamei1023@users.noreply.github.com>
2022-12-15 00:10:00 +0900

    usersテーブルに保存されるようにした

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,14 +8,14 @@ export const Header = memo(({ isMenu, setMenu }: any) => {
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center">
           <a href="https://flowbite.com" className="flex items-center">
-            <img
+            {/* <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="mr-3 h-6 sm:h-9"
               alt="Flowbite Logo"
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Flowbite
-            </span>
+            </span> */}
           </a>
           <div className="flex items-center lg:order-2">
             <a
--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flowbite.s3.amazonaws.com",
+        port: "",
+        pathname: "/blocks/marketing-ui/avatars/sofia-mcguire.png",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
